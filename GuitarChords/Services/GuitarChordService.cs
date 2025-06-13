13973cc0711f7b8a1dd83aaa7526b1d074802c9e
Philippe Matray <phmatray@gmail.com>
2025-06-14 01:44:48 +0200

    feat(chord-explorer): enhance chord exploration interface with search and filter options

--- GuitarChords/Services/GuitarChordService.cs
+++ GuitarChords/Services/GuitarChordService.cs
@@ -98,6 +98,105 @@ public class GuitarChordService
             BaseFret = 0
         };
         CommonChords["Dm"] = dMinor;
+
+        // Cm (C minor) - barre chord
+        var cMinor = new GuitarChord(new Chord(new Note(NoteName.C, Alteration.Natural, 4), ChordType.Minor))
+        {
+            FretPositions = new[] { -1, 3, 5, 5, 4, 3 },
+            Fingerings = new[] { -1, 1, 3, 4, 2, 1 },
+            BaseFret = 3,
+            IsBarreChord = true,
+            BarreFret = 3,
+            BarredStrings = new List<int> { 1, 5 }
+        };
+        CommonChords["Cm"] = cMinor;
+
+        // Gm (G minor) - barre chord
+        var gMinor = new GuitarChord(new Chord(new Note(NoteName.G, Alteration.Natural, 3), ChordType.Minor))
+        {
+            FretPositions = new[] { 3, 5, 5, 3, 3, 3 },
+            Fingerings = new[] { 1, 3, 4, 1, 1, 1 },
+            BaseFret = 3,
+            IsBarreChord = true,
+            BarreFret = 3,
+            BarredStrings = new List<int> { 0, 3, 4, 5 }
+        };
+        CommonChords["Gm"] = gMinor;
+
+        // Fm (F minor) - barre chord
+        var fMinor = new GuitarChord(new Chord(new Note(NoteName.F, Alteration.Natural, 3), ChordType.Minor))
+        {
+            FretPositions = new[] { 1, 3, 3, 1, 1, 1 },
+            Fingerings = new[] { 1, 3, 4, 1, 1, 1 },
+            BaseFret = 1,
+            IsBarreChord = true,
+            BarreFret = 1,
+            BarredStrings = new List<int> { 0, 3, 4, 5 }
+        };
+        CommonChords["Fm"] = fMinor;
+
+        // Bm (B minor) - simplified version
+        var bMinor = new GuitarChord(new Chord(new Note(NoteName.B, Alteration.Natural, 3), ChordType.Minor))
+        {
+            FretPositions = new[] { -1, 2, 4, 4, 3, 2 },
+            Fingerings = new[] { -1, 1, 3, 4, 2, 1 },
+            BaseFret = 2
+        };
+        CommonChords["Bm"] = bMinor;
+
+        // G7 (G dominant 7th)
+        var g7 = new GuitarChord(new Chord(new Note(NoteName.G, Alteration.Natural, 3), ChordType.Major))
+        {
+            FretPositions = new[] { 3, 2, 0, 0, 0, 1 },
+            Fingerings = new[] { 3, 2, 0, 0, 0, 1 },
+            BaseFret = 0
+        };
+        CommonChords["G7"] = g7;
+
+        // C7 (C dominant 7th)
+        var c7 = new GuitarChord(new Chord(new Note(NoteName.C, Alteration.Natural, 4), ChordType.Major))
+        {
+            FretPositions = new[] { -1, 3, 2, 3, 1, 0 },
+            Fingerings = new[] { -1, 3, 2, 4, 1, 0 },
+            BaseFret = 0
+        };
+        CommonChords["C7"] = c7;
+
+        // D7 (D dominant 7th)
+        var d7 = new GuitarChord(new Chord(new Note(NoteName.D, Alteration.Natural, 4), ChordType.Major))
+        {
+            FretPositions = new[] { -1, -1, 0, 2, 1, 2 },
+            Fingerings = new[] { -1, -1, 0, 2, 1, 3 },
+            BaseFret = 0
+        };
+        CommonChords["D7"] = d7;
+
+        // A7 (A dominant 7th)
+        var a7 = new GuitarChord(new Chord(new Note(NoteName.A, Alteration.Natural, 3), ChordType.Major))
+        {
+            FretPositions = new[] { -1, 0, 2, 0, 2, 0 },
+            Fingerings = new[] { -1, 0, 2, 0, 3, 0 },
+            BaseFret = 0
+        };
+        CommonChords["A7"] = a7;
+
+        // E7 (E dominant 7th)
+        var e7 = new GuitarChord(new Chord(new Note(NoteName.E, Alteration.Natural, 3), ChordType.Major))
+        {
+            FretPositions = new[] { 0, 2, 0, 1, 0, 0 },
+            Fingerings = new[] { 0, 2, 0, 1, 0, 0 },
+            BaseFret = 0
+        };
+        CommonChords["E7"] = e7;
+
+        // B7 (B dominant 7th)
+        var b7 = new GuitarChord(new Chord(new Note(NoteName.B, Alteration.Natural, 3), ChordType.Major))
+        {
+            FretPositions = new[] { -1, 2, 1, 2, 0, 2 },
+            Fingerings = new[] { -1, 2, 1, 3, 0, 4 },
+            BaseFret = 0
+        };
+        CommonChords["B7"] = b7;
     }
 
     public GuitarChord? GetChord(string chordSymbol)
