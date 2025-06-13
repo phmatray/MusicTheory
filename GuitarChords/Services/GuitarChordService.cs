dcfb49a530688be73ce30d817243851be82897d6
Philippe Matray <phmatray@gmail.com>
2025-06-14 01:19:34 +0200

    feat(chord-diagram): adjust dot positioning and fingerings for G and A major chords

--- GuitarChords/Services/GuitarChordService.cs
+++ GuitarChords/Services/GuitarChordService.cs
@@ -28,7 +28,7 @@ public class GuitarChordService
         var gMajor = new GuitarChord(new Chord(new Note(NoteName.G, Alteration.Natural, 3), ChordType.Major))
         {
             FretPositions = new[] { 3, 2, 0, 0, 3, 3 },
-            Fingerings = new[] { 3, 2, 0, 0, 4, 4 },
+            Fingerings = new[] { 2, 1, 0, 0, 3, 4 },
             BaseFret = 0
         };
         CommonChords["G"] = gMajor;
@@ -46,7 +46,7 @@ public class GuitarChordService
         var aMajor = new GuitarChord(new Chord(new Note(NoteName.A, Alteration.Natural, 3), ChordType.Major))
         {
             FretPositions = new[] { -1, 0, 2, 2, 2, 0 },
-            Fingerings = new[] { -1, 0, 2, 3, 4, 0 },
+            Fingerings = new[] { -1, 0, 1, 2, 3, 0 },
             BaseFret = 0
         };
         CommonChords["A"] = aMajor;
